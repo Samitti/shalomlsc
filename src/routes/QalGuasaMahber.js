89a676aa8c7f77bbbaff3fd7d31a83731b4613cd
Samitti <samuelteweldebrhan@gmail.com>
2022-12-06 10:47:19 +0300

    add pastor message page and styled it

--- src/routes/QalGuasaMahber.js
+++ src/routes/QalGuasaMahber.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import HeroimgAll from '../components/HeroimgAll'
+import QalGuasaMahberDetails from '../components/QalGuasaMahberDetails'
 
 function QalGuasaMahber() {
   return (
     <div>
       <Navbar />
       <HeroimgAll heading="ቃል ጓሳ ማሕበር" />
+      <QalGuasaMahberDetails />
       <Footer />
     </div>
   )
